38c0588266a2259021b473da747719bb51473314
TranDucThanh932000 <56164088+TranDucThanh932000@users.noreply.github.com>
2022-09-03 09:37:22 +0700

    first commit be

---
+++ fe-blog/src/App.vue
@@ -0,0 +1,129 @@
+<template>
+  <v-app>
+    <!-- <v-navigation-drawer app>
+    </v-navigation-drawer> -->
+
+    <v-app-bar app color="primary lighten-1">
+      <v-spacer></v-spacer>
+      <template v-for="menu in menuHeader">
+        <MenuHeader :items="menu.children" :key="menu.id" :name="menu.name" :offsetx="false" :offsety="true"></MenuHeader>
+      </template>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid style="height: 100%">
+        <v-row>
+          <v-col md="8" offset-md="1">
+            <router-view style="height: 100%"></router-view>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+    </v-footer>
+    <Loading ref="loading"> </Loading>
+    <toast-message ref="toastMessage" />
+  </v-app>
+</template>
+
+<script>
+import Loading from "@/components/Loading";
+import ToastMessage from "@/components/ToastMessage";
+import MenuHeader from "@/components/MenuHeader.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Loading,
+    ToastMessage,
+    MenuHeader
+  },
+  data() {
+    return{
+      menuHeader: [
+        {
+          name: 'AAAAA',
+          id: 1,
+          children: [
+            {
+              name: 'AAAAA1',
+              id: 2,
+              children: []
+            },
+            {
+              name: 'AAAAA2',
+              id: 2,
+              children: []
+            }
+          ]
+        },
+        {
+          name: 'AAAAA',
+          id: 2,
+          children: [
+            {
+              name: 'AAAAA1',
+              id: 2,
+              children: []
+            },
+            {
+              name: 'AAAAA2',
+              id: 2,
+              children: []
+            }
+          ]
+        },
+        {
+          name: 'AAAAA3',
+          id: 3,
+          children: [
+            {
+              name: 'AAAAA31',
+              id: 2,
+              children: []
+            },
+            {
+              name: 'AAAAA32',
+              id: 10,
+              children: [
+                  {
+                    name: 'AAAAA3331',
+                    id: 11,
+                    children: []
+                  },
+                  {
+                    name: 'AAAAA3332',
+                    id: 12,
+                    children: []
+                  },
+              ]
+            }
+          ]
+        },
+        {
+          name: 'AAAAA4',
+          id: 4,
+          children: []
+        },
+        {
+          name: 'AAAAA',
+          id: 5,
+          children: [
+            {
+              name: 'AAAAA1',
+              id: 2,
+              children: []
+            },
+            {
+              name: 'AAAAA2',
+              id: 2,
+              children: []
+            }
+          ]
+        },
+      ]
+    }
+  }
+};
+</script>
---
+++ fe-blog/src/components/MenuHeader.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <v-menu close-on-content-click :offset-y="offsety" :offset-x="offsetx">
+          <template v-slot:activator="{ on }">
+            <v-btn v-if="items.length > 0" v-on="on" text>
+                {{ name }}
+                <v-spacer></v-spacer>
+                <v-icon>mdi-chevron-right</v-icon>
+            </v-btn>
+            <v-list-item color="green" v-on="on" v-else>
+                {{ name }}
+            </v-list-item>
+          </template>
+          <v-list v-if="items.length > 0">
+            <div v-for="(item, index) in items" :key='index'>
+              <MenuHeader
+                v-if="item.children.length > 0"
+                :items="item.children"
+                :name="item.name"
+                :offsety="false"
+                :offsetx="true"
+              ></MenuHeader>
+              <v-list-item v-else>
+                <v-list-item-title>{{ item.name }}</v-list-item-title>
+              </v-list-item>
+            </div>
+          </v-list>
+        </v-menu>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuHeader',
+  props: ['items', 'name', 'offsetx', 'offsety']
+}
+</script>
